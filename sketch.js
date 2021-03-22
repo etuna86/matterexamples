01abc081256753c4518b71dbc869dfa4fed331de
erol tuna <eroltuna86@gmail.com>
2021-03-22 17:00:25 +0300

    cisim hızları arttırlmalı

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ var Engine = Matter.Engine,
 
 // create an engine
 var engine = Engine.create();
-
+engine.world.gravity.y = 0;
 // create a renderer
 var render = Render.create({
     element: document.body,
@@ -15,11 +15,24 @@ var render = Render.create({
 });
 
 // create two boxes and a ground
-var ball = Bodies.circle(100, 50, 40,{ isStatic: true });
+var ball = Bodies.circle(60, 50, 40,{ isStatic: false });
+//var ball2 = Bodies.circle(100, 100, 40,{ isStatic: false });
+//var ball = Bodies.circle(400, 50, 40,{ isStatic: false });
 
-var racket = Bodies.rectangle(10, 50, 10, 80,{ isStatic: true });
+var racket = Bodies.rectangle(10, 100, 10, 80,{ isStatic: true });
+//var racket = Bodies.rectangle(400, 510, 80, 10,{ isStatic: true });
 
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+/*
+var sling=Matter.Constraint.create({
+    pointA:{x:200,y:200},
+    bodyB:ball2,
+    stiffness:0.5,
+});  */
+
+var top = Bodies.rectangle(0, 0, 810, 60, { isStatic: true });
+var left = Bodies.rectangle(0, 0, 810, 60, { isStatic: true });
+var bottom = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+var right = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
 
 
@@ -52,9 +65,9 @@ document.addEventListener('keydown', (event) => {
         console.warn("racket", racket.position.y);
     }
     else if(event.key=="Enter"){
-        Body.rotate( racket, Math.PI/6);
+        Body.rotate( racket, Math.PI/2);
         setTimeout(function(){
-            Body.rotate( racket, -( Math.PI/6));
+        Body.rotate( racket, -( Math.PI/2));
         },100)
         
     }
@@ -63,7 +76,7 @@ document.addEventListener('keydown', (event) => {
 
 
 // add all of the bodies to the world
-World.add(engine.world, [ball, racket, ground]);
+World.add(engine.world, [ball, racket, top,left,bottom,right]);
 
 // run the engine
 Engine.run(engine);
